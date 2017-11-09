e26107bd8c97616d813592dd7c19ed052659193f
GERARDO <GERARDO@DESKTOP-JE5SO9B>
2017-11-09 14:21:24 -0600

    Consumir una WebService

--- webservice/Form1.cs
+++ webservice/Form1.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using webservice.ServiceReference1;
 using System.Windows.Forms;
+using System.IO;
 
 namespace webservice
 {
@@ -15,6 +17,17 @@ namespace webservice
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GlobalWeatherSoapClient obj = new GlobalWeatherSoapClient();
+            string resultado = obj.GetCitiesByCountry(textBox1.Text);
+            DataSet resul = new DataSet();
+            StringReader lectura = new StringReader(resultado);
+            resul.ReadXml(lectura);
+            comboBox1.DataSource = resul.Tables[0];
+            comboBox1.DisplayMember = "city";
         }
     }
 }
