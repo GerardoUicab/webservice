56acd2116c139546dada89db56e63a4483f8db70
GERARDO <GERARDO@DESKTOP-JE5SO9B>
2017-11-10 11:53:05 -0600

    dato

---
+++ WebServiceRest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WebServiceRest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cargar = "http://jsonplaceholder.typicode.com/posts";
+            var json = new WebClient().DownloadString(cargar);
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject(json, (typeof(DataTable)));
+            label1.Text = json;
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
